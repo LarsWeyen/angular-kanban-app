8bed1f08132b1cf40fce35cd7c34a8aea5808a4f
LarsWeyen <97236276+LarsWeyen@users.noreply.github.com>
2024-02-29 15:25:39 +0100

    feat: add the task to the corresponding board

--- src/app/board/board.service.ts
+++ src/app/board/board.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { asyncScheduler, scheduled } from 'rxjs';
 import { Board } from '../models/board';
 import { Column } from '../models/column';
+import { Task } from '../models/task';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,9 @@ import { Column } from '../models/column';
 export class BoardService {
   private data: Board[] = [
     new Board(1,'Platform Launch',[
-        {columnId: 1, columnTitle: 'TODO'},
-        {columnId: 2, columnTitle: 'DOING'},
-        {columnId: 3, columnTitle: 'Done'},
+        {columnId: 1, columnTitle: 'TODO', tasks: []},
+        {columnId: 2, columnTitle: 'DOING', tasks: []},
+        {columnId: 3, columnTitle: 'Done', tasks: []},
     ]),
     new Board(2,'Marketing Plan',[]),
     new Board(3,'Roadmap',[])
@@ -24,9 +25,20 @@ export class BoardService {
   
   addColumnToBoard(boardId: number, newColumn: Column){
     const boardIndex = this.data.findIndex(board => board.boardId === boardId);
-    console.log(boardIndex)
+
     if (boardIndex !== -1) {
       this.data[boardIndex].boardColumns.push(newColumn);
+    } else {
+      console.error(`Board with ID ${boardId} not found.`);
+    }
+  }
+
+  addTaskToColumn(boardId: number, newTask: Task){
+    const boardIndex = this.data.findIndex(board => board.boardId === boardId);
+    const columnIndex = this.data[boardIndex].boardColumns.findIndex(column => column.columnId === newTask.column.columnId)
+
+    if (boardIndex !== -1) {
+      this.data[boardIndex].boardColumns[columnIndex].tasks.push(newTask);
     } else {
       console.error(`Board with ID ${boardId} not found.`);
     }
