898694eae7e8e03cff3bdf1e7a0843099883a16e
LarsWeyen <97236276+LarsWeyen@users.noreply.github.com>
2024-03-01 16:34:51 +0100

    feat: toggle iscomplete of subtask

--- src/app/board/board.service.ts
+++ src/app/board/board.service.ts
@@ -3,6 +3,7 @@ import { asyncScheduler, scheduled } from 'rxjs';
 import { Board } from '../models/board';
 import { Column } from '../models/column';
 import { Task } from '../models/task';
+import { Subtask } from '../models/subtask';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,9 @@ import { Task } from '../models/task';
 export class BoardService {
   private data: Board[] = [
     new Board(1,'Platform Launch',[
-        {columnId: 1, columnTitle: 'TODO', tasks: []},
+        {columnId: 1, columnTitle: 'TODO', tasks: [
+          {title: 'test',description:'test',subtasks:[],column:{columnId:1,columnTitle: 'TODO',tasks:[]}}
+        ]},
         {columnId: 2, columnTitle: 'DOING', tasks: []},
         {columnId: 3, columnTitle: 'Done', tasks: []},
     ]),
@@ -50,5 +53,18 @@ export class BoardService {
 
   getBoard(id: number){
     return scheduled([this.data.find(b => b.boardId === id)],asyncScheduler);
+  }
+
+  toggleCompletedSubtask(boardId: number, task: Task, subtask: Subtask){
+    const boardIndex = this.data.findIndex(board => board.boardId === boardId);
+    const columnIndex = this.data[boardIndex].boardColumns.findIndex(column => column.columnId === task.column.columnId)
+    const taskIndex = this.data[boardIndex].boardColumns[columnIndex].tasks.findIndex(t => t.title === task.title)
+    const subtaskIndex = this.data[boardIndex].boardColumns[columnIndex].tasks[taskIndex].subtasks.findIndex(sub=> sub.title === subtask.title)
+
+    if (boardIndex !== -1) {
+      this.data[boardIndex].boardColumns[columnIndex].tasks[taskIndex].subtasks[subtaskIndex].isComplete = !this.data[boardIndex].boardColumns[columnIndex].tasks[taskIndex].subtasks[subtaskIndex].isComplete;
+    } else {
+      console.error(`Board with ID ${boardId} not found.`);
+    }
   }
 }
